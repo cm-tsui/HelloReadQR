47a247c0bf096af5b02b236078b2c64eab991e8f
Joe Tsui <cmtsui@gmail.com>
2022-01-29 01:20:28 +0800

    Add folder and file

---
+++ app.py
@@ -0,0 +1,22 @@
+import cv2
+# initalize the cam
+cap = cv2.VideoCapture(0)
+# initialize the cv2 QRCode detector
+detector = cv2.QRCodeDetector()
+while True:
+    _, img = cap.read()
+    # detect and decode
+    data, bbox, _ = detector.detectAndDecode(img)
+    # check if there is a QRCode in the image
+    if bbox is not None:
+        # display the image with lines
+        for i in range(len(bbox)):
+            # draw all lines
+            cv2.line(img, tuple(bbox[0][i].astype('int32')), tuple(bbox[0][(i+1) % len(bbox)].astype('int32')), color=(255, 0, 0), thickness=2)
+        print("[+] QR Code detected, data:", data)
+    # display the result
+    cv2.imshow("img", img)
+    if cv2.waitKey(1) == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
